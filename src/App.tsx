36f51d8ee04123055e37f62462854eb6d0a37bac
30isnottoolate <akos.varga.92@gmail.com>
2022-10-08 16:53:56 +0200

    Reformat code

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div id="app">
-      Hello!
-    </div>
-  );
+	return (
+		<div id="app">
+			Hello!
+		</div>
+	);
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('app-container') as HTMLElement
+	document.getElementById('app-container') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
 );
